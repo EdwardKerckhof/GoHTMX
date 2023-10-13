98255c0956848b92ae49a60fadb6a267673c5303
EdwardKerckhof <edwardkerckhof@hotmail.com>
2023-10-13 16:20:52 +0200

    [feat]: jwt maker

---
+++ pkg/token/jwt/jwt.maker.go
@@ -0,0 +1,57 @@
+package jwt
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/EdwardKerckhof/gohtmx/pkg/token"
+	"github.com/golang-jwt/jwt"
+)
+
+const (
+	minSecretKeySize = 32
+)
+
+type JWTMaker struct {
+	secretKey string
+}
+
+func NewJWTMaker(secretKey string) (token.Maker, error) {
+	if len(secretKey) < minSecretKeySize {
+		return nil, fmt.Errorf("invalid key size: must be at least %d characters", minSecretKeySize)
+	}
+	return &JWTMaker{secretKey}, nil
+}
+
+func (m *JWTMaker) GenerateToken(username string, duration time.Duration) (string, error) {
+	payload := token.NewPayload(username, duration)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	return token.SignedString([]byte(m.secretKey))
+}
+
+func (m *JWTMaker) VerifyToken(t string) (*token.Payload, error) {
+	keyFunc := func(jwtToken *jwt.Token) (interface{}, error) {
+		_, ok := jwtToken.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, token.ErrInvalidToken
+		}
+		return []byte(m.secretKey), nil
+	}
+
+	jwtToken, err := jwt.ParseWithClaims(t, &token.Payload{}, keyFunc)
+	if err != nil {
+		validationErr, ok := err.(*jwt.ValidationError)
+		if ok && errors.Is(validationErr.Inner, token.ErrExpiredToken) {
+			return nil, token.ErrExpiredToken
+		}
+		return nil, token.ErrInvalidToken
+	}
+
+	payload, ok := jwtToken.Claims.(*token.Payload)
+	if !ok {
+		return nil, token.ErrInvalidToken
+	}
+
+	return payload, nil
+}
---
+++ pkg/token/jwt/jwt.maker_test.go
@@ -0,0 +1,67 @@
+package jwt
+
+import (
+	"testing"
+	"time"
+
+	"github.com/EdwardKerckhof/gohtmx/pkg/token"
+	"github.com/golang-jwt/jwt"
+	"github.com/stretchr/testify/require"
+)
+
+func TestJWTMaker(t *testing.T) {
+	secret := "12345678901234567890123456789012"
+	maker, err := NewJWTMaker(secret)
+	require.NoError(t, err)
+
+	username := "test"
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	jwt, err := maker.GenerateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, jwt)
+
+	payload, err := maker.VerifyToken(jwt)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+}
+
+func TestExpiredJWT(t *testing.T) {
+	secret := "12345678901234567890123456789012"
+	maker, err := NewJWTMaker(secret)
+	require.NoError(t, err)
+
+	username := "test"
+	jwt, err := maker.GenerateToken(username, -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, jwt)
+
+	payload, err := maker.VerifyToken(jwt)
+	require.Error(t, err)
+	require.EqualError(t, err, token.ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
+
+func TextInvalidJWTAlgNone(t *testing.T) {
+	payload := token.NewPayload("test", time.Minute)
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodNone, payload)
+	tok, err := jwtToken.SignedString(jwt.UnsafeAllowNoneSignatureType)
+	require.NoError(t, err)
+
+	secret := "12345678901234567890123456789012"
+	maker, err := NewJWTMaker(secret)
+	require.NoError(t, err)
+
+	payload, err = maker.VerifyToken(tok)
+	require.Error(t, err)
+	require.EqualError(t, err, token.ErrInvalidToken.Error())
+	require.Nil(t, payload)
+}
